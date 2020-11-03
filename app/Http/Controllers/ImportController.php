83836290d469f8b99ef6cdcdf1c9b79c800d1697
Minh Luan Le <leminhluan4244@gmail.com>
2020-11-04 01:51:56 +0700

    Update gói cài đặt

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -2,16 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\IndexImport;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class ImportController extends Controller
 {
     public function index(){
+        $inputFileName = public_path().'\import.xlsx';
+        $this->importProduct($inputFileName);
+    }
 
-        $listSheet = Excel::import(new IndexImport, 'import.xlsx');
-        dd($listSheet);
-        // return redirect('/')->with('success', 'All good!');
+    private function importProduct($inputFileName){
+        $spreadsheet = IOFactory::load($inputFileName);
+        $sheet = $spreadsheet->getSheetByName('DMHH');
+        $insertParams = array();
+        $rowData = $sheet->rangeToArray('C8:H1000', null, true, false);
+        foreach($rowData as $row){
+            if(null == $row[0]){
+                break;
+            } else {
+                $insertParams[] = array(
+                    'product_code' => $row[0],
+                    'product_name' => $row[1],
+                    'product_unit' => $row[2],
+                    'product_amount' => $row[3],
+                    'product_input_price' => $row[4],
+                    'product_sale_price' => $row[5],
+                );
+            }
+        }
+        DB::transaction(function () use ($insertParams) {
+            Product::truncate();
+            Product::insert($insertParams);
+        });
     }
 }
